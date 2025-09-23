f3c92adc17a443449c653d554f78d39bdc7efa0e
Samineni Mokshagna <moksharam4s@gmail.com>
2025-09-23 09:49:38 +0530

    Add files via upload

---
+++ two sum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        k = []
+
+        for i in len(nums):
+            req = target - i
+        if req in nums:
+            return k=req[i],n
+        return k
+
+        
